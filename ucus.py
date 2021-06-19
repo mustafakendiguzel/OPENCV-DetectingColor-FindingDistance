4d01a1a88893c3267c119b432729c665bdfee865
Mustafa Kendigüzel <72809479+mustafakendiguzel@users.noreply.github.com>
2021-06-20 02:13:51 +0300

    Updated 1.1fix

--- ucus.py
+++ ucus.py
@@ -1,9 +1,7 @@
-
-
 import cv2
 import numpy as np
+import math
 from width_distance import * 
-
 
 kam = cv2.VideoCapture(0)
 foto = cv2.imread('resim.jpg')
@@ -11,7 +9,7 @@ foto = cv2.imread('resim.jpg')
 def foto_cevirme(fotogir):
     hsv = cv2.cvtColor(fotogir,cv2.COLOR_BGR2HSV)
 
-    fotogir = cv2.blur(fotogir,(3,3))
+    fotogir = cv2.blur(fotogir,(14,13))
 
 
     lower_red = np.array([0,120,70])
@@ -41,6 +39,7 @@ def foto_cevirme(fotogir):
 
     res1 = cv2.bitwise_and(fotogir,fotogir,mask=mask1)
     cv2.imshow("foto",fotogir)
+
  
 def focal_length(measured_distance, real_width, width_in_rf_image):
    focal_length = (width_in_rf_image * measured_distance)/ real_width
@@ -60,28 +59,22 @@ fonts = cv2.FONT_HERSHEY_COMPLEX
 while True:
   
   ret, goruntu = kam.read()
-
+  goruntu = cv2.flip(goruntu,1)
 
   hsv = cv2.cvtColor(goruntu,cv2.COLOR_BGR2HSV)
-
-  goruntu = cv2.blur(goruntu,(3,3))
-
-
+    
+  goruntu = cv2.blur(goruntu,(1,1))
   lower_red = np.array([0,120,70])
-
-  upper_red = np.array([10,255,255])
+  upper_red = np.array([10,255,255])  # önceden 10du
 
   mask1 = cv2.inRange(hsv, lower_red, upper_red)
-
-
   lower_red = np.array([170,120,70])
-
   upper_red = np.array([180,255,255])
 
   mask2 = cv2.inRange(hsv,lower_red,upper_red)
+  mask1 = mask1 + mask2
+  genis = 1
 
- 
-  mask1 = mask1+mask2
   (contours,hierarchy) = cv2.findContours(mask1.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
   for c in contours:
     if cv2.contourArea(c) < 500:
@@ -90,23 +83,21 @@ while True:
     cv2.rectangle(goruntu,(x,y),(x+w,y+h),(255,0,0),3)
     genis = w
     
-
-
   
   Distance = distance_finder(focal_length_found,known_width,genis)
-  cv2.putText(goruntu,f"Distance = {Distance}",(50,50),fonts,1,(0,0,255),2)
-
+  if Distance > 200:
+    Distance /= 100
+    cv2.putText(goruntu,f"Uzaklik = {math.trunc(Distance)}m",(50,50),fonts,1,(0,0,255),2)
+  else:
+    cv2.putText(goruntu,f"Uzaklik = {(Distance)}cm",(50,50),fonts,1,(0,0,255),2)
+  
   res1 = cv2.bitwise_and(goruntu,goruntu,mask=mask1)
-
-  stackImages
-  cv2.imshow("orj",goruntu)
+  a = cv2.imshow("orj",goruntu)
   cv2.imshow("mask",mask1)
   cv2.imshow("res",res1)
   
-  
   if cv2.waitKey(30) & 0xFF == ord('q'):
     break
-    
 
 kam.release()
 cv2.destroyAllWindows()
